c9d3ccf899e8929dcb7d65a57f613aff1b6f8c18
Marsgoat <johnny3681@gmail.com>
2023-12-25 15:14:57 +0800

    91. Decode Ways

---
+++ Dynamic_Programming/91.Decode-Ways/91.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int numDecodings(string s) {
+    int n = s.size();
+    if (n == 0 || s[0] == '0') return 0;
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      int oneDigit = s[i - 1] - '0';
+      int twoDigits = (s[i - 2] - '0') * 10 + oneDigit;
+
+      if (oneDigit >= 1) {
+        dp[i] += dp[i - 1];
+      }
+      if (twoDigits >= 10 && twoDigits <= 26) {
+        dp[i] += dp[i - 2];
+      }
+    }
+
+    return dp[n];
+  }
+};
+
+int main() {
+  Solution solution;
+  string s = "226";
+  cout << solution.numDecodings(s) << endl;
+  return 0;
+}
