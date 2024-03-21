b16c3fbc664b885ed46741db9880dcab8b1b29b1
Marsgoat <johnny3681@gmail.com>
2024-03-21 15:55:01 +0800

    206. Reverse Linked List

--- Linked_List/206.Reverse-Linked-List/206.cpp
+++ Linked_List/206.Reverse-Linked-List/206.cpp
@@ -22,6 +22,14 @@ class Solution {
 
     return prev;
   }
+
+  ListNode* reverseList2(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+    ListNode* p = reverseList2(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return p;
+  }
 };
 
 int main() {
