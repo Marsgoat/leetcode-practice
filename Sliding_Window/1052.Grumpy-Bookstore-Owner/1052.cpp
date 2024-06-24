e846d952da897cc3f4b0c2259a1ec099020ab1dc
Marsgoat <johnny3681@gmail.com>
2024-06-24 14:31:47 +0800

    1052. Grumpy Bookstore Owner

--- Sliding_Window/1052.Grumpy-Bookstore-Owner/1052.cpp
+++ Sliding_Window/1052.Grumpy-Bookstore-Owner/1052.cpp
@@ -5,7 +5,27 @@ using namespace std;
 
 class Solution {
  public:
-  int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {}
+  int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+    int result = 0;
+    int right = 0;
+    int count = 0;
+
+    for (int i = 0; i < customers.size(); i++) {
+      if (grumpy[i] == 0) {
+        result += customers[i];
+      } else {
+        count += customers[i];
+      }
+
+      if (i >= minutes) {
+        count -= grumpy[i - minutes] * customers[i - minutes];
+      }
+
+      right = max(right, count);
+    }
+
+    return result + right;
+  }
 };
 
 int main() {
